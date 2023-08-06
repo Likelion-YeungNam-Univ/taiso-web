aaf932532b8bf6b90492bf0004b59f77671510b8
Subi Hwang <ghkdtnql@gmail.com>
2023-08-06 10:39:59 +0900

    login second  commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
-import SignInPage from './pages';
+// import {BrowserRouter, Route, Routes } from 'react-router-dom'
+// import SignInPage from './pages';
+
+// function App() {
+//   return (
+//     <BrowserRouter> 
+//       <Routes>
+//        <Route path="/login" element={<SignInPage />}/>
+//         {/* <Route path="/register" element={<RegisterPage />}/> */}
+//         {/* <Route path="/" element={<MainPage />} /> */}
+//         {/* <Route path="/*" element={<NotFoundPage />}/> */} {/* 404 Not Found */}
+//       </Routes>
+//   </BrowserRouter>
+//   );
+// }
+
+
+import SignInPage from "./pages/auth/SignInPage";
+// import NaverLogin from "./pages/auth/NaverLogin";
 
 function App() {
   return (
-    <BrowserRouter> 
-      <Routes>
-       <Route path="/login" element={<SignInPage />}/>
-        {/* <Route path="/register" element={<RegisterPage />}/> */}
-        {/* <Route path="/" element={<MainPage />} /> */}
-        {/* <Route path="/*" element={<NotFoundPage />}/> */} {/* 404 Not Found */}
-      </Routes>
-  </BrowserRouter>
+    <div className="App">
+    <div>
+      <SignInPage />
+    </div>
+  </div>
   );
-}
+};
 
 export default App;
--- src/pages/auth/SignInPage.js
+++ src/pages/auth/SignInPage.js
@@ -1,33 +1,67 @@
-// import { Link } from "react-router-dom";
- //import logo from './assets/LoginFormLogo.jpg'
-
 import React from "react";
-//import { SignInBox } from "styles/SignContainer.styled";
-// //import {useNavigate} from 'react-router-dom';
-// import { SignInForm } from "components/Auth";
+import tireLogo from "../../assets/tireLogo.jpg"; 
+import naverLogo from "../../assets/naverLogo.png"; 
+import kakaoLogo from "../../assets/kakaoLogo.png"; 
+import googleLogo from "../../assets/googleLogo.png"; 
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TireLogoStyle = styled.img`
+  width: 265px;
+  height: 75px;
+`;
+
+const LoginBtnStyle = styled.div`
+display: flex;
+align-items: center;
+`;
+
+const BtnSytle = styled.button`
+
+//color: ${({ theme }) => theme.White};
+
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 56px;
+  background-color: white;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+`;
+
 
 const SignInPage = () => {
-    //const navigate = useNavigate();
 
-    return (
-                <div>
-                    <h1>더 이상 추천받지 않는 안전을 위하여, </h1>
-                    <p>가장 먼저 보여지는 페이지입니다.</p>
-                    //<img src={process.env.PUBLIC_URL + '/LoginFormLogo.jpg'} width = '300px'/>
-                </div>
-            );
-}
+
+  return (
+    <Container>
+      <h1>더 이상 추천받지 않는 안전을 위하여,</h1>
+      <TireLogoStyle src={tireLogo} alt = "로고" />
+      <BtnSytle>
+      <LoginBtnStyle>
+        <button>
+        <img src={naverLogo} alt = "네이버 로고" />
+      </button>
+    </LoginBtnStyle>
+    <LoginBtnStyle>
+        <button>
+        <img src={kakaoLogo} alt = "카카오 로고" />
+      </button>
+    </LoginBtnStyle>
+    <LoginBtnStyle>
+        <button>
+        <img src={googleLogo} alt = "구글 로고" />
+        </button>
+    </LoginBtnStyle>
+    </BtnSytle>
+    </Container>
+  );
+};
 
 export default SignInPage;
-
-// function SignInPage () {
-//     return (
-//         <div>
-//             <h1>더 이상 추천받지 않는 안전을 위하여, </h1>
-//             <p>가장 먼저 보여지는 페이지입니다.</p>
-//             <img src={process.env.PUBLIC_URL + '/LoginFormLogo.jpg'} width = '300px'/>
-//         </div>
-//     );
-// }
-
-// export default SignInPage;
