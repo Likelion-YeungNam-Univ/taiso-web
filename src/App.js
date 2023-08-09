fcd9be183662efe091ac51c761bbda832f17b0e6
KangRok Yoon <ykr0919@naver.com>
2023-08-10 01:33:04 +0900

    style maonpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { Header, TopBar, Footer} from "components";
 import { MainPage } from "pages";
 
-// import RecommandBoard from "components/main/recommand/RecommandBoard";
-
-// import SignInPage from "./pages/auth/SignInPage";
 
 
-import RecommandBoard from "components/main/recommand/RecommandBoard";
-import SignInPage from "./pages/auth/SignInPage";
-import ReviewBoard from "components/main/review/ReviewBoard";
 
-function App() {
+
+
+
+const App = () => {
   return (
     <div className="App">
     <div>
@@ -23,7 +20,6 @@ function App() {
       <MainPage/>
     </div>
       <Footer/>
-      <ReviewBoard/>
     </div>
   
   );
