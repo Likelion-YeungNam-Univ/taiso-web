b78931457c5c222ed223493546bff6a93ac575b4
Subi Hwang <ghkdtnql@gmail.com>
2023-08-14 00:10:09 +0900

    Mypage Third commit

--- src/components/mypage/Mybookmark.js
+++ src/components/mypage/Mybookmark.js
@@ -9,12 +9,24 @@ flex-Direction: row;
 width:1168px;
 margin: 0 auto;
 justify-content: space-between;
+margin-bottom:20px;
 `
 const Content = styled.div`
-flex-Direction: column;
+ display:flex;
+ margin-top:10px;
 `
+const OutContent = styled.div`
+width:1200px;
+h1{
+  border-bottom: 1px solid #e0e0e0; /* 밑줄 추가 */
+}
+`
+const InnerContent = styled.div`
+margin-left:15px;
+`
+
 const Title = styled.div`
-margin-top:5px;
+margin-left:15px;
 p{
 color: #484848;
 font-family: IBM Plex Sans KR;
@@ -28,13 +40,20 @@ line-height: normal;
 function Mybookmark() {
     return (
       <Container>
-        <MyProfile/>
+        <OutContent>
+        <h1> 마이 페이지 </h1>
         <Content>
+        <MyProfile/>
+        {/* <Content> */}
+        <InnerContent>
         <Title><p>만나서 반갑습니다, 진우님!</p></Title>
-            <Mycar/>
-            <MybookmarkBoard/>
-        </Content> 
-      </Container>
+             <Mycar/>
+              <MybookmarkBoard/>
+        </InnerContent>
+         </Content> 
+         </OutContent>
+       </Container>
+      // </OutContent>
     );
   }
 
